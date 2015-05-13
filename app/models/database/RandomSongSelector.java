70e8649e66dd6b958fc8f845c234ca0b765b5b04
Stefan Boodt <S.A.Boodt@student.tudelft.nl>
2015-05-13 10:32:16 +0200

    Started with random song selection. Actual database usage still remains not implemented at this point in this branch. This will change in a future commit.
    The current lack of database usage by this commit is due to the fact that the database code is currently being updated in the database branch. However this code should give a clear indication on how the random song is going to be collected.

---
+++ app/models/database/RandomSongSelector.java
@@ -0,0 +1,75 @@
+package models.database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * This class selects a random song from the database. The current version of
+ * this class uses the singleton pattern.
+ * 
+ * @since 13-05-2015
+ * @version 13-05-2015
+ * 
+ * @see DatabaseConnector
+ * 
+ * @author stefanboodt
+ *
+ */
+public class RandomSongSelector {
+
+	/**
+	 * The Singleton selector that is used by this class.
+	 */
+	private static RandomSongSelector selector;
+
+	/**
+	 * Private constructor that construct new random song selectors.
+	 */
+	private RandomSongSelector() {
+
+	}
+
+	/**
+	 * Method that returns the track_id of a random song in the database.
+	 * @return The track_id of a random song.
+	 */
+	public int getRandomSong() {
+		final String trackid = "track_id";
+		final String subquery = "(SELECT DISTINCT track_id FROM comments_without_features)";
+		final String query = "SELECT " + trackid + " FROM " + subquery
+				+ " ORDER BY RAND() LIMIT 1";
+		ResultSet result = null;
+		try {
+			return result.getInt(trackid);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error when doing query "
+			+ query + " on the database", e);
+		}
+	}
+
+	/**
+	 * Retrieves a new random song selector to allow usage of the
+	 * {@link #getRandomSong()} method.
+	 * @return A new RandomSongSelector or the already created one if one
+	 * already existed.
+	 */
+	public static RandomSongSelector getRandomSongSelector() {
+		if (selector == null) {
+			selector = new RandomSongSelector();
+		}
+		return selector;
+	}
+
+	/**
+	 * This method returns the track_id of a random song in the database. This
+	 * method is equivalent to {@link #getRandomSongSelector()} and then use
+	 * {@link #getRandomSong()} on it. This method therefore is only a
+	 * convenience method.
+	 * @return The track_id of a random song.
+	 */
+	public static int getRandomSongTrackId() {
+		return getRandomSongSelector().getRandomSong();
+	}
+
+}
