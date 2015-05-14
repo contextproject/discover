8f95c8770bd37cfcd10758f8dcc56496494f5f8c
Daan Schipper <daancschipper@gmail.com>
2015-05-14 17:01:22 +0200

    Added javadoc to the global class

--- app/models/Global.java
+++ app/models/Global.java
@@ -5,10 +5,22 @@ import play.Application;
 import play.GlobalSettings;
 import play.Logger;
 
+/**
+ * The global class of the play application
+ */
 public class Global extends GlobalSettings {
 
+    /**
+     * DatabaseConnector object to the database
+     */
     public DatabaseConnector databaseConnector = new DatabaseConnector();
 
+    /**
+     * The method that gets called on the start of the play application.
+     * The connection to the database gets established here.
+     *
+     * @param app The play application
+     */
     public void onStart(Application app) {
         String url = "jdbc:mysql://188.166.78.36/contextbase";
         String username = "context";
@@ -20,6 +32,12 @@ public class Global extends GlobalSettings {
         controllers.Application.setDatabaseConnector(databaseConnector);
     }
 
+    /**
+     * The method that gets called on the stop of the play application.
+     * The connection to the database gets terminated here.
+     *
+     * @param app The play application
+     */
     public void onStop(Application app) {
         Logger.info("Application shutdown...");
 
