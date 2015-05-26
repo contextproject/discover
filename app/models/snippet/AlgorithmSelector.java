35fd7aa5cc04473fd22a5cefb3297cf9536383a8
Daan Schipper <daancschipper@gmail.com>
2015-05-26 10:43:15 +0200

    refactor of the existing code

--- app/models/snippet/AlgorithmSelector.java
+++ app/models/snippet/AlgorithmSelector.java
@@ -1,88 +1,86 @@
 package models.snippet;
 
+import models.database.CommentRetriever;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-import models.database.CommentRetriever;
 /**
  * Selects which algorithm could be used best and also combines algorithms.
  */
 public class AlgorithmSelector {
 
-	/**
-	 * The track id of the current song.
-	 */
-	private final int trackid;
-	
-	/**
-	 * A variable which indicates if a song has enough comments.
-	 */
-	private boolean enoughComments = false;
+    /**
+     * The track id of the current song.
+     */
+    private final int trackid;
 
-	/**
-	 * A comment retriever object to use.
-	 */
-	private CommentRetriever retriever;
+    /**
+     * A variable which indicates if a song has enough comments.
+     */
+    private boolean enoughComments = false;
 
-	/**
-	 * Constructor for making a AlgorithmSelector object.
-	 * 
-	 * @param trackid
-	 *            the id of a song.
-	 */
-	public AlgorithmSelector(final int trackid) {
-		this.trackid = trackid;
-		retriever = new CommentRetriever();
-		int commentAmount = retriever.getAmountOfComments(trackid);
-		if (commentAmount > 5) {
-			enoughComments = true;
-		}
+    /**
+     * A comment retriever object to use.
+     */
+    private CommentRetriever retriever;
 
-	}
-	
-	/**
-	 * This function decides which algorithm to use and computes a snippet in differentways.
-	 * @return the snippet for the start time and duration of a Snippet.
-	 */
-	public TimedSnippet getSnippet() {
-		if (enoughComments) {
+    /**
+     * Constructor for making a AlgorithmSelector object.
+     *
+     * @param trackid the id of a song.
+     */
+    public AlgorithmSelector(final int trackid) {
+        this.trackid = trackid;
+        retriever = new CommentRetriever();
+        int commentAmount = retriever.getAmountOfComments(trackid);
+        if (commentAmount > 5) {
+            enoughComments = true;
+        }
+    }
 
-			HashMap<Comment, String> map = (HashMap<Comment, String>) retriever
-					  .getCommentsWithString(trackid);
-			Set<Comment> set = processContent(map);
-			CommentIntensitySeeker cis = new CommentIntensitySeeker();
-			TimedSnippet ts = cis.seek(set);
+    /**
+     * This function decides which algorithm to use and computes a snippet in different ways.
+     *
+     * @return the snippet for the start time and duration of a Snippet.
+     */
+    public TimedSnippet getSnippet() {
+        if (enoughComments) {
+            HashMap<Comment, String> map = (HashMap<Comment, String>) retriever
+                    .getCommentsWithString(trackid);
+            Set<Comment> set = processContent(map);
+            CommentIntensitySeeker cis = new CommentIntensitySeeker();
+            TimedSnippet ts = cis.seek(set);
 
-			return ts;
+            return ts;
+        } else {
+            return new TimedSnippet(0, TimedSnippet.getDefaultDuration());
+        }
+    }
 
-		} else {
-			return new TimedSnippet(0,TimedSnippet.getDefaultDuration());
-		}
-	}
+    /**
+     * Processes the content of the comments retrieved with CommentRetriever.
+     *
+     * @param map the hash map with the comments and their content.
+     * @return A set of comments
+     */
+    private Set<Comment> processContent(final HashMap<Comment, String> map) {
+        HashSet<Comment> result = new HashSet<Comment>();
+        ContentFilter cf = new ContentFilter();
 
-	/**
-	 * Processes the content of the comments retrieved with CommentRetriever.
-	 * @param map the hash map with the comments and their content.
-	 * @return A set of comments
-	 */
-	private Set<Comment> processContent(final HashMap<Comment, String> map) {
+        for (Comment c : map.keySet()) {
+            String s = map.get(c);
+            if (cf.contentFilter(s)) {
+                result.add(c);
+            }
+        }
 
-		HashSet<Comment> result = new HashSet<Comment>();
-		ContentFilter cf = new ContentFilter();
-
-		for (Comment c : map.keySet()) {
-			String s = map.get(c);
-			if (cf.contentFilter(s)) {
-				result.add(c);
-			}
-		}
-		
-		if (result.size() > 5) {
-			return result;
-		} else {
-			return map.keySet();
-		}
-	}
+        if (result.size() > 5) {
+            return result;
+        } else {
+            return map.keySet();
+        }
+    }
 
 }
--- app/models/snippet/ContentFilter.java
+++ app/models/snippet/ContentFilter.java
@@ -2,62 +2,60 @@ package models.snippet;
 
 /**
  * Filters the content of a comment.
- *
  */
 public class ContentFilter {
 
-	/**
-	 * List of positive words.
-	 */
-	private final String[] positive = { "great", "brilliant", "beautiful",
-		"awesome", "amazing", "perfect", "good", "like", "love", "best",
-		"nice", "super", "cool", "massive", "wicked", "sick" };
+    /**
+     * List of positive words.
+     */
+    private final String[] positive = {"great", "brilliant", "beautiful",
+            "awesome", "amazing", "perfect", "good", "like", "love", "best",
+            "nice", "super", "cool", "massive", "wicked", "sick"};
 
-	/**
-	 * List of negative words.
-	 */
-	private final String[] negative = { "hate", "n't", "nt", "not", "bad",
-		"terrible", "worst" };
+    /**
+     * List of negative words.
+     */
+    private final String[] negative = {"hate", "n't", "nt", "not", "bad",
+            "terrible", "worst"};
 
-	/**
-	 * Checks if a string contains a word from the list above.
-	 * 
-	 * @param content
-	 *            the content of a comment
-	 * @return true if the content contains a positive message
-	 */
-	public boolean contentFilter(final String content) {
-		String body = content.toLowerCase();
-		boolean res = false;
-		for (String p : positive) {
-			if (body.contains(p)) {
-				res = true;
-			}
-		}
+    /**
+     * Checks if a string contains a word from the list above.
+     *
+     * @param content the content of a comment
+     * @return true if the content contains a positive message
+     */
+    public boolean contentFilter(final String content) {
+        String body = content.toLowerCase();
+        boolean res = false;
+        for (String p : positive) {
+            if (body.contains(p)) {
+                res = true;
+                break;
+            }
+        }
 
-		for (String n : negative) {
-			if (body.contains(n)) {
-				return false;
-			}
-		}
+        for (String n : negative) {
+            if (body.contains(n)) {
+                return false;
+            }
+        }
 
-		if (res) {
-			return res;
-		} else {
-			return findEmoticons(body);
-		}
-	}
+        if (res) {
+            return res;
+        } else {
+            return findEmoticons(body);
+        }
+    }
 
-	/**
-	 * Checks if the content of a comment contains a positive emoticon.
-	 * 
-	 * @param body
-	 *            content of a comment
-	 * @return true if the content contains a happy emoticon
-	 */
-	public boolean findEmoticons(final String body) {
-		return body.contains(":)") || body.contains("<3")
-				|| body.contains(":d") || body.contains(":-)")
-				|| body.contains("=d");
-	}
+    /**
+     * Checks if the content of a comment contains a positive emoticon.
+     *
+     * @param body content of a comment
+     * @return true if the content contains a happy emoticon
+     */
+    public boolean findEmoticons(final String body) {
+        return body.contains(":)") || body.contains("<3")
+                || body.contains(":d") || body.contains(":-)")
+                || body.contains("=d");
+    }
 }
