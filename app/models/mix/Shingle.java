6923bc4ee9ad4de6165db077556e2ba222cb3aeb
Stefan Boodt <S.A.Boodt@student.tudelft.nl>
2015-05-26 10:51:44 +0200

    Did some formatting, created a new test for the Jaccard distance and updated the jaccard distance method in Shingle so it can handle empty shingles.
    Empty shingle do no longer return NaN as jaccard distance.

--- app/models/mix/Shingle.java
+++ app/models/mix/Shingle.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 /**
  * The ShingleSet class contains a sorted set of shingles. *
+ * 
+ * @version 26-05-2015
  */
 public class Shingle {
 
@@ -25,11 +27,42 @@ public class Shingle {
 	 * @param other The other set of shingles that this set will be compared to.
 	 * @return The Jaccard distance between this set and the other set.
 	 */
-	public double jaccardDistance(Shingle other) {
+	public double jaccardDistance(final Shingle other) {
 		Shingle part = this;
 		part.getData().retainAll(other.getData());
-		double ans = (double) part.size() / (double) this.size();
-		return 1-ans; 
+		final Shingle union = union(other);
+		final double unionsize = union.size();
+		final double ans;
+		// Some safety for the double rounding errors.
+		// Variable created to prevent magic numbers.
+		final double lessThanOne = 0.9;
+		if (unionsize < lessThanOne) {
+		    ans = 1.0;
+		} else {
+		    ans = (double) part.size() / unionsize;
+		}
+		return 1.0 - ans;
+	}
+	
+	/**
+	 * Unionizes two Shingles. This method uses a functional way of using
+	 * to ensure that both of the shingles are unaltered.
+	 * @param other The other shingle.
+	 * @return The union of the two shingles.
+	 */
+	public Shingle union(final Shingle other) {
+	    List<Float> union = new ArrayList<Float>(this.size() + other.size());
+	    for (Float f: data) {
+	        if (!union.contains(f)) {
+                union.add(f);
+            }
+	    }
+	    for (Float f: other.getData()) {
+	        if (!union.contains(f)) {
+                union.add(f);
+            }
+        }
+	    return new Shingle(union);
 	}
 
 	public int size() {
--- test/models/mix/MixSplitterTest.java
+++ test/models/mix/MixSplitterTest.java
@@ -24,107 +24,126 @@ import static org.junit.Assert.assertEquals;
  */
 public class MixSplitterTest extends BasicTest {
 
-	/*
-	 * temporary turn CHECKSTYLE:OFF because of the default trackid
-	 * that is initialized here.
-	 */
-	
-	/**
-	 * Default trackid of the tested mixsplitter.
-	 */
-	protected static int DEFAULT_TRACKID;
-	
-	// Turn the CHECKSTYLE:ON again for the rest of the file.
-	
-	/**
-	 * The splitter under test.
-	 */
-	private MixSplitter splitter;
-	
-	/**
-	 * Does some set up for the class.
-	 * @throws Exception If the set up fails.
-	 */
-	@Override
-	public void setUp() throws Exception {
-		super.setUp();
-		DEFAULT_TRACKID = 2431002;
-		setSplitter(new MixSplitter(asList(), DEFAULT_TRACKID));
-	}
-	
-	/**
-	 * Returns the given numbers as a list.
-	 * @param numbers The numbers to have in the list.
-	 * @return The list containing the given numbers.
-	 */
-	protected List<Float> asList(final float ... numbers) {
-		List<Float> floats = new ArrayList<Float>(numbers.length);
-		for (float f : numbers) {
-			floats.add(f);
-		}
-		return floats;
-	}
+    /*
+     * temporary turn CHECKSTYLE:OFF because of the default trackid that is
+     * initialized here.
+     */
 
-	/**
-	 * Sets the splitter under test.
-	 * @param splitter The new tested splitter.
-	 */
-	public void setSplitter(final MixSplitter splitter) {
-		super.setObjectUnderTest(splitter);
-		this.splitter = splitter;
-	}
-	
-	/**
-	 * Gets the splitter under test.
-	 * @return The splitter that is being tested.
-	 */
-	public MixSplitter getSplitter() {
-		return splitter;
-	}
+    /**
+     * Default trackid of the tested mixsplitter.
+     */
+    protected static int DEFAULT_TRACKID;
 
-	/**
-	 * Tests the {@link MixSplitter#split()} method on an empty dataset.
-	 */
-	@Test
-	public void testSplitEmpty() {
-		assertEquals(new ArrayList<Integer>(), getSplitter().split());
-	}
-	
-	/**
-	 * Tests the {@link MixSplitter#getTrackID()} method.
-	 */
-	@Test
-	public void testGetTrackID() {
-		assertEquals(DEFAULT_TRACKID, getSplitter().getTrackID());
-	}
-	
-	/**
-	 * Tests the {@link MixSplitter#getTrackID()} method.
-	 */
-	@Test
-	public void testGetTrackIDAfterSetting() {
-		final int id = 2104921042;
-		final MixSplitter split = getSplitter();
-		split.setTrackID(id);
-		assertEquals(id, split.getTrackID());
-	}
-	
-	/**
-	 * Tests the {@link MixSplitter#getData()} method.
-	 */
-	@Test
-	public void testDataEmpty() {
-		assertEquals(asList(), getSplitter().getData());
-	}
-	
-	/**
-	 * Tests the {@link MixSplitter#getData()} method.
-	 */
-	@Test
-	public void testData() {
-		final MixSplitter split = getSplitter();
-		final List<Float> data = asList(4.0f, 2.1f, 3.9f);
-		split.setData(data);
-		assertEquals(data, split.getData());
-	}
+    // Turn the CHECKSTYLE:ON again for the rest of the file.
+
+    /**
+     * The splitter under test.
+     */
+    private MixSplitter splitter;
+
+    /**
+     * Does some set up for the class.
+     * 
+     * @throws Exception
+     *             If the set up fails.
+     */
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        DEFAULT_TRACKID = 2431002;
+        setSplitter(new MixSplitter(asList(), DEFAULT_TRACKID));
+    }
+
+    /**
+     * Returns the given numbers as a list.
+     * 
+     * @param numbers
+     *            The numbers to have in the list.
+     * @return The list containing the given numbers.
+     */
+    protected List<Float> asList(final float... numbers) {
+        List<Float> floats = new ArrayList<Float>(numbers.length);
+        for (float f : numbers) {
+            floats.add(f);
+        }
+        return floats;
+    }
+
+    /**
+     * Sets the splitter under test.
+     * 
+     * @param splitter
+     *            The new tested splitter.
+     */
+    public void setSplitter(final MixSplitter splitter) {
+        super.setObjectUnderTest(splitter);
+        this.splitter = splitter;
+    }
+
+    /**
+     * Gets the splitter under test.
+     * 
+     * @return The splitter that is being tested.
+     */
+    public MixSplitter getSplitter() {
+        return splitter;
+    }
+
+    /**
+     * Tests the {@link MixSplitter#split()} method on an empty dataset.
+     */
+    @Test
+    public void testSplitEmpty() {
+        assertEquals(new ArrayList<Integer>(), getSplitter().split());
+    }
+
+    /**
+     * Tests the {@link MixSplitter#getTrackID()} method.
+     */
+    @Test
+    public void testGetTrackID() {
+        assertEquals(DEFAULT_TRACKID, getSplitter().getTrackID());
+    }
+
+    /**
+     * Tests the {@link MixSplitter#getTrackID()} method.
+     */
+    @Test
+    public void testGetTrackIDAfterSetting() {
+        final int id = 2104921042;
+        final MixSplitter split = getSplitter();
+        split.setTrackID(id);
+        assertEquals(id, split.getTrackID());
+    }
+
+    /**
+     * Tests the {@link MixSplitter#getData()} method.
+     */
+    @Test
+    public void testDataEmpty() {
+        assertEquals(asList(), getSplitter().getData());
+    }
+
+    /**
+     * Tests the {@link MixSplitter#getData()} method.
+     */
+    @Test
+    public void testData() {
+        final MixSplitter split = getSplitter();
+        final List<Float> data = asList(4.0f, 2.1f, 3.9f);
+        split.setData(data);
+        assertEquals(data, split.getData());
+    }
+
+    /**
+     * Tests the {@link MixSplitter#getJaccardDistance(List, List)} method.
+     */
+    @Test
+    public void testGetJaccardDistanceEmpty() {
+        final List<Float> empty = new ArrayList<Float>();
+        final double expected = 0.0;
+        final double delta = 0.01;
+        assertEquals(expected, getSplitter().getJaccardDistance(empty, empty),
+                delta);
+    }
 }
