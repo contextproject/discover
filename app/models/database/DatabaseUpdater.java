a7719d169b32b68063044c6e4927a50f5863990d
ahovanesyan <a.hovanesyan@gmail.com>
2015-06-10 16:16:54 +0200

    Added basic methods to the DatabaseUpdater

--- app/models/database/DatabaseUpdater.java
+++ app/models/database/DatabaseUpdater.java
@@ -1,5 +1,42 @@
 package models.database;
 
+import models.record.Track;
+
 public class DatabaseUpdater {
 
+    private DatabaseUpdater instance;
+    
+    private String query = "INSERT IGNORE INTO tracks (,,,,) VALUES (,,,,)";
+    
+    private DatabaseConnector dbconnector;
+    
+    private DatabaseUpdater() {
+        this.dbconnector = DatabaseConnector.getConnector();
+    }
+    
+    public DatabaseUpdater getInstance() {
+        if(instance == null) {
+            instance = new DatabaseUpdater(); 
+        }
+        return instance;
+    }
+    
+    public void updateIfNeeded(Track track) {
+        check(track);
+    }
+    
+    private void check(Track track) {
+        update(track);
+        insert(track);
+    }
+
+    private void update(Track track) {
+        
+    }
+    
+    private void insert(Track track) {
+        this.dbconnector.executeQuery(query);
+    }
+    
+    
 }
