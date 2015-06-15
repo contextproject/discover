c474528c96f253fd64ac34e4e32aafbd4934d7f4
ahovanesyan <a.hovanesyan@gmail.com>
2015-06-15 11:13:54 +0200

    The class will use a StringBuilder create the query

--- app/models/database/DatabaseUpdater.java
+++ app/models/database/DatabaseUpdater.java
@@ -1,19 +1,24 @@
 package models.database;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
+import controllers.Application;
 import models.record.Track;
 
 public class DatabaseUpdater {
 
     private static DatabaseUpdater instance;
 
-    private String query = "INSERT IGNORE INTO tracks (,,,,) VALUES (,,,,)";
+    private StringBuilder query ; // = "INSERT IGNORE INTO tracks (,,,,) VALUES (,,,,)";
 
     private DatabaseConnector dbconnector;
 
     private DatabaseUpdater() {
         this.dbconnector = DatabaseConnector.getConnector();
+        query = new StringBuilder();
     }
 
     public static DatabaseUpdater getInstance() {
@@ -28,12 +33,19 @@ public class DatabaseUpdater {
     }
 
     public String buildQuery(JsonNode json) {
+        StringBuilder res = new StringBuilder();
         String query = "INSERT IGNORE INTO tracks ";
         String colums = "(track_id, created_at, "
                         + "user_id, duration, commentable, streamable, " //6
-                        + "downloadable, genre, release_name, label_name, track_type, bpm, title, " //7
-                        + "release_year, release_month, release_day, original_content_size, license, " //5
+                        + "downloadable, genre, label_name, track_type, bpm, title, " //7
+                        + " original_content_size, license, " //5
                         + "playback_count, favoritings_count, download_count, comment_count) "; //4
+        res.append("INSERT IGNORE INTO tracks ");
+        res.append("(track_id, created_at, "
+                        + "user_id, duration, commentable, streamable, " //6
+                        + "downloadable, genre, label_name, track_type, bpm, title, " //7
+                        + " original_content_size, license, " //5
+                        + "playback_count, favoritings_count, download_count, comment_count) ");
         String values = "VALUES (" + 
                         json.get("id").asInt() + ", '" + 
                         json.get("created_at").asText() + "', " + 
@@ -43,20 +55,21 @@ public class DatabaseUpdater {
                         json.get("streamable").asBoolean() + ", " + 
                         json.get("downloadable").asBoolean()+ ", '" + 
                         json.get("genre").asText() + "', " + 
-                        null + ", '" + 
                         json.get("label_name").asText() + "', '" + 
                         json.get("track_type").asText() + "', " + 
                         json.get("bpm").asDouble() + ", '" + 
                         json.get("title").asText()+ "', " + 
-                        null + ", " + 
-                        null + ", " +  
-                        null + ", '" + 
                         json.get("original_content_size").asText() + "', '" + 
                         json.get("license").asText() + "', '" + 
                         json.get("playback_count").asText() + "', '" + 
                         json.get("favoritings_count").asText() + "', " +
                         json.get("download_count").asInt() + ", " + 
                         json.get("comment_count").asInt() +")";
+        try {
+            PreparedStatement ps = Application.getDatabaseConnector().getConnection().prepareStatement(res.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return query + colums + values;
     }
 
@@ -69,6 +82,6 @@ public class DatabaseUpdater {
     // }
 
     private void insert(Track track) {
-        this.dbconnector.executeQuery(query);
+        this.dbconnector.executeQuery(query.toString());
     }
 }
