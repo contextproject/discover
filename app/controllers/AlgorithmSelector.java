5a189130302f53ec071bdaa52a8e65bd6069eea4
ahovanesyan <a.hovanesyan@gmail.com>
2015-06-15 10:48:36 +0200

    Fixed the checkstyle errors in the AlgorithmSelector

--- app/controllers/AlgorithmSelector.java
+++ app/controllers/AlgorithmSelector.java
@@ -9,10 +9,36 @@ import models.seeker.RandomSeeker;
  */
 public final class AlgorithmSelector {
 
+    /**
+     * The Mode of the AlgorithmSelector that dictates its choice.
+     */
     public enum Mode {
-        AUTO, INTENSITY, CONTENT, RANDOM
+        /**
+         * Mode: AUTO. The selector will make the choice between every algorithm
+         * himself.
+         */
+        AUTO,
+        /**
+         * Mode: INTENSITY. The selector will choose the intensity seeker to
+         * calculate the preview.
+         */
+        INTENSITY,
+        /**
+         * Mode: CONTENT. The selector will choose the intensity seeker along
+         * with the to calculate the preview.
+         */
+        CONTENT,
+
+        /**
+         * Mode: RANDOM. The selector will choose a random time to start based
+         * on its duration.
+         */
+        RANDOM
     }
 
+    /**
+     * 
+     */
     private static Mode curMode = AlgorithmSelector.Mode.AUTO;
 
     /**
@@ -25,18 +51,18 @@ public final class AlgorithmSelector {
     public static int determineStart(final Track track) {
         int start;
         switch (curMode) {
-        case INTENSITY:
-            start = commentIntensity(track);
-            break;
-        case RANDOM:
-            start = random(track);
-            break;
-        default:
-            start = commentIntensity(track);
-            if (start == 0) {
+            case CONTENT:
+                start = commentContent(track);
+                break;
+            case RANDOM:
                 start = random(track);
-            }
-            break;
+                break;
+            default:
+                start = commentContent(track);
+                if (start == 0) {
+                    start = random(track);
+                }
+                break;
         }
         return start;
     }
@@ -45,21 +71,21 @@ public final class AlgorithmSelector {
      * Determine the start of the snippet based on the intensity of the
      * comments.
      *
-     * @param track
-     *            The track
+     * @param track The track
      * @return The start of the snippet
      */
     private static int commentIntensity(final Track track) {
-        return new CommentIntensitySeeker(track).seek().getStartTime();
+        return 0;
     }
 
     /**
      * Determine the start of the snippet based on the content of the comments.
-     *
+     * 
+     * @param track The track
      * @return The start of the snippet
      */
-    private static int commentContent() {
-        return 0;
+    private static int commentContent(final Track track) {
+        return new CommentIntensitySeeker(track).seek().getStartTime();
     }
 
     /**
@@ -73,9 +99,14 @@ public final class AlgorithmSelector {
         return new RandomSeeker(track).seek().getStartTime();
     }
 
-    public static void setMode(String mode) {
-        if (mode.equalsIgnoreCase("intensity")) {
-            curMode = Mode.INTENSITY;
+    /**
+     * Sets the mode of the Algorithm Selector.
+     * 
+     * @param mode String representation of the new Mode.
+     */
+    public static void setMode(final String mode) {
+        if (mode.equalsIgnoreCase("content")) {
+            curMode = Mode.CONTENT;
         } else if (mode.equalsIgnoreCase("random")) {
             curMode = Mode.RANDOM;
         } else {
