79ad00cf8090a0eb2ddcc7f6ff0a56ec8cce0970
Daan Schipper <daancschipper@gmail.com>
2015-06-18 10:57:02 +0200

    The searched tracks are displayed nicely and when clicking a track the widget loads it now

--- public/javascripts/tracks.js
+++ public/javascripts/tracks.js
@@ -1,14 +1,96 @@
+// This file handles the search bar and displays the results of the search bar and recommended tracks.
+
+$("#reload").click(function () {
+    analyzeInput();
+});
+
+$("#url").keypress(function (e) {
+    if (e.which == 13) {
+
+        analyzeInput();
+    }
+});
+
+$("#tracks").on('click', '.item', function () {
+    reloadWidget($(this).attr("value"));
+});
+
+$("#searchList").on('click', '.item', function () {
+    reloadWidget($(this).attr("value"));
+    $("#searchList").html("");
+});
+
+function analyzeInput() {
+    var input = $("#url").val();
+    if (input.charAt(0) == "\/") {
+        reloadWidget("w.soundcloud.com/tracks" + input);
+    } else {
+        search(input);
+    }
+    clearInput();
+}
+
+function clearInput() {
+    $("#url").val("");
+}
+
+function search(input) {
+    SC.get('/tracks', {q: input, limit: 5}, function (tracks) {
+        append(tracks, $("#searchList"));
+    });
+}
+
 $("#gettracks").click(function () {
-    widget.getSounds(function (data) {
-        $.getJSON("/tracks", function (data) {
-            jQuery.each(data, function (i, val) {
-                $("#tracks").append("<li class='track' value='" + val.url + "'>"
-                    + val.artist + " - " + val.title + "</li>");
-            });
-        });
+    SC.get('/tracks', {q: 'buuren', limit: 5}, function (tracks) {
+        append(tracks, $("#tracks"));
     });
 });
 
-$("#tracks").on('click', '.track', function () {
-    reloadWidget($(this).attr("value"));
-});
+// reload the widget with the url submitted in the input field
+function reloadWidget(url) {
+    mixSplits = null;
+    // load the url in the widget
+    widget.load(url, {
+        auto_play: false,
+        likes: true
+    });
+    widget.bind(SC.Widget.Events.READY, function () {
+        widget.unbind(SC.Widget.Events.READY);
+        var pos;
+        widget.getCurrentSoundIndex(function (index) {
+            pos = index;
+        });
+        widget.getSounds(function (sounds) {
+            var message = {
+                "track": sounds[pos],
+                "waveform": waveform.data
+            };
+            sendData(message, "/request", setStartTime);
+            // use the following line if you want to re-render the page.
+            // window.location.href = "http://localhost:9000/tracks/" + sounds[pos].id;
+        });
+    });
+}
+
+function append(tracks, element) {
+    element.val("");
+    jQuery.each(tracks, function (i, track) {
+        element.append(
+            "<li class='list_item' >" +
+            "<div class='row 150% item' value=" + track.permalink_url + ">" +
+            "<div class='2u 12u'>" +
+            "<span class='album-art'>" +
+            "<img src=" + track.artwork_url +
+            "</span> " +
+            "</div>" +
+            "<div class='10u 12u content'>" +
+
+            "<div class='user'><h6>" + track.user.username + "</h6></div>" +
+            "<div class='title'><h5>" + track.title + "</h5></div>" +
+
+            "</div>" +
+            "</div>" +
+            "</li>"
+        );
+    });
+}
